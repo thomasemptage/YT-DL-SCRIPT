02e3a248b1b04a970f42bb05d5724e39d857fcc4
Tom <thomas.emptage@gmail.com>
2022-06-05 23:30:10 +0100

    Update Download.sh
    General improvements.

--- Download.sh
+++ Download.sh
@@ -1,2 +1,5 @@
 dir=$PWD
-yt-dlp -S "height:480" --merge-output mkv --embed-metadata --download-archive $DIR\archive.txt -r 10M -a $DIR\input.txt
+yt-dlp -S "height:480" --merge-output mkv --embed-metadata --download-archive $DIR\archive.txt -r 10M -a $DIR\input.txt -o "%(uploader)s/%(title)s - %(id)s.%(ext)s"
+# Download vidoes from "input.txt" in 480p, merge to a mkv container,embed metadata,add video ID's to archive, rate limit to 10Mb, output to channel specific folder.
+sh message.sh
+#Execute telegram bot message
