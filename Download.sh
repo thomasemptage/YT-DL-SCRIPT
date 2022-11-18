abffb71305e96291b4c233019354b3c060673bb2
Tom <thomas.emptage@gmail.com>
2022-11-18 17:14:06 +0000

    Update Download.sh

--- Download.sh
+++ Download.sh
@@ -1,5 +1,5 @@
-dir=$PWD
-yt-dlp -S "height:480" --merge-output mkv --embed-metadata --download-archive $DIR\archive.txt -r 10M -a $DIR\input.txt -o "%(uploader)s/%(title)s - %(id)s.%(ext)s"
-# Download vidoes from "input.txt" in 480p, merge to a mkv container,embed metadata,add video ID's to archive, rate limit to 10Mb, output to channel specific folder.
+# Download vidoes from "input.txt" in 480p, merge to a mkv container, skip youtube dash formats, embed metadata, add video ID's to archive file, rate limit to 10Mb, output to channel specific folder, and prefix the file name with the video ID.
+yt-dlp -S "height:480" --merge-output mkv --extractor-args "youtube:skip=dash" --embed-metadata --no-mtime --write-description --download-archive "archive.txt" -r 10M -a "input.txt" -o "Youtube Archive/%(uploader)s/%(upload_date)s - %(title)s - %(id)s.%(ext)s"
 sh message.sh
 #Execute telegram bot message
+
